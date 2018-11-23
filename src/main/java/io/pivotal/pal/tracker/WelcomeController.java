339054f0a5f76276e0f1e240c3b28c505a4c33eb
Harshitha Phaniraj <harshitha_phaniraj@dell.com>
2018-11-23 15:16:05 +0530

    Adding Assignment 2 commit

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,13 +1,19 @@
 package io.pivotal.pal.tracker;
 
+        import org.springframework.beans.factory.annotation.Value;
         import org.springframework.web.bind.annotation.GetMapping;
         import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
+    private final String msg;
+
+    public WelcomeController(@Value("${WELCOME_MESSAGE}") String message) {
+        this.msg = message;
+    }
 
     @GetMapping("/")
     public String sayHello() {
-        return "hello";
+        return msg;
     }
 }
