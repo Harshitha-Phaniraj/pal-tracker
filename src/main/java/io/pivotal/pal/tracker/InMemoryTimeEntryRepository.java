804dd221a75c09218dcd4ad844a619fdc29a53d0
Harshitha Phaniraj <harshitha_phaniraj@dell.com>
2018-11-29 17:08:41 +0530

    Adding changes for rest application assignment

---
+++ src/main/java/io/pivotal/pal/tracker/InMemoryTimeEntryRepository.java
@@ -0,0 +1,39 @@
+package io.pivotal.pal.tracker;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class InMemoryTimeEntryRepository implements TimeEntryRepository {
+    private HashMap<Long, TimeEntry> timeEntries = new HashMap<>();
+
+    @Override
+    public TimeEntry create(TimeEntry timeEntry) {
+        Long id = timeEntries.size() + 1L;
+        TimeEntry newTimeEntry = new TimeEntry(id, timeEntry.getProjectId(), timeEntry.getUserId(), timeEntry.getDate(), timeEntry.getHours());
+        timeEntries.put(id, newTimeEntry);
+        return newTimeEntry;
+    }
+
+    @Override
+    public TimeEntry find(long id) {
+        return timeEntries.get(id);
+    }
+
+    @Override
+    public TimeEntry update(long id, TimeEntry timeEntry) {
+        TimeEntry updateTimeEntry = new TimeEntry(id, timeEntry.getProjectId(), timeEntry.getUserId(), timeEntry.getDate(), timeEntry.getHours());
+        timeEntries.replace(id, updateTimeEntry);
+        return updateTimeEntry;
+    }
+
+    @Override
+    public List<TimeEntry> list() {
+        return new ArrayList<>(timeEntries.values());
+    }
+
+    @Override
+    public void delete(long id) {
+        timeEntries.remove(id);
+    }
+}
